1ae75f8f429acbba34f01d1dbefe00ace85707ee
Adyssa Fairuz Husniyyah <adyssafairuzhusniyyah@Adyssas-MacBook-Air.local>
2025-03-21 10:42:05 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "adyssa";
+        int x = 5;
+        int y = 6;
+        x = 9;
+        int sum = x + y;
+        System.out.println("Hello, " + name);
+        System.out.println("Result: " + sum);
+    }
+}
